57f0d707ab5be09d0fc4f8f404301a9ecf6ee6cc
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-11-18 17:49:47 -0300

    config storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,17 @@
+const config = {
+  staticDirs: ['../public'],
+  stories: ['../src/components/**/stories.tsx'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  },
+  webpackFinal(config) {
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  }
+}
+export default config
