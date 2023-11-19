49c83118143c0005f0acf26f9bba15b7c0881c98
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2023-11-19 14:55:12 -0300

    config storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,9 +1,19 @@
 import * as S from './styles'
 
-export function Main() {
-  return (
-    <S.wrapper>
-      <h1>React Avançado</h1>
-    </S.wrapper>
-  )
-}
+export const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
+  </S.wrapper>
+)
